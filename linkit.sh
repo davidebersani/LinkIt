bdd1445594fdf83649c2f3168f9666e4f2cd1992
Antonin Décimo <antonin.decimo@gmail.com>
2014-09-03 22:15:02 +0200

    Correct many typos and grammar errors. Improve readability, fix indentation

--- linkit.sh
+++ linkit.sh
@@ -1,89 +1,96 @@
 #!/bin/sh
-#This scirpt allow to create a launcher for the menù. This launcher will added to your menù and also to your desktop, if you want.
-#For other information: https://github.com/bersani96/LinkIt
-########################## Variables ##############################
-prog="Link It"
+
+# This script allows you to create a launcher for the menu.
+# The launcher will be added to your menu and also to your desktop, if you want to.
+# For other information: https://github.com/bersani96/LinkIt
+
+## Variables
+prog="LinkIt"
 conf="/etc/linkit/linkit.conf"
-############################ Functions #############################
 
-set_borwser(){
-	read b < $conf
+## Functions
+set_browser(){
+    read b < $conf
 }
 
-#Create .desktop file (starter for the menù)
+# Create .desktop file (launcher for the menu)
 file_desktop(){
-        echo "[Desktop Entry]" >>$nome.desktop
-        echo "Encoding=UTF-8" >>$nome.desktop
-        echo "Type=Application" >>$nome.desktop
-        echo "Version=1.0" >>$nome.desktop
-        echo "Name=$nome" >>$nome.desktop
-        #Check if the user want to use a custom icon
-        zenity --question --title="$prog" --text="Do you want to use a custom icon?\nIf no, a default icon will be used."
-        if [ $? -eq 0 ]
-        then
-                #Choose the icon
-                icon=`zenity --file-selection --title="Choose the icon"`
-                echo "Icon=$icon" >>$nome.desktop
-        else
-                #Else, i use a random icon
-                echo "Icon=application-default-icon" >>$nome.desktop
-        fi
-        choose=`zenity --list --title="$prog" \
-	  --text="What does the launcher should open?" \
-	  --column="Options :" \
-			"Comand" \
-			"URL address"`
-		
-		case $choose in
-		"Comand")
-			dato=`zenity --entry --title="$prog" --text="Insert the command"`
-        	echo "Exec=$dato" >>$nome.desktop
-        ;;
+    echo "[Desktop Entry]"  >> $name.desktop
+    echo "Encoding=UTF-8"   >> $name.desktop
+    echo "Type=Application" >> $name.desktop
+    echo "Version=1.0"      >> $name.desktop
+    echo "Name=$name"       >> $name.desktop
+
+    # Check if the user want to use a custom icon
+    zenity --question --title="$prog" --text="Do you want to use a custom icon ?\nIf not, a default icon will be used."
+    if [ $? -eq 0 ]
+    then
+        # Choose the icon
+        icon=`zenity --file-selection --title="Choose the icon"`
+        echo "Icon=$icon" >> $name.desktop
+    else
+        # Else use a default icon
+        echo "Icon=application-default-icon" >> $name.desktop
+    fi
+    choose=`zenity --list --title="$prog" \
+        --text="What does the launcher should open ?" \
+        --column="Options :" \
+        "Command" \
+        "URL address"`
+
+    case $choose in
+        "Command")
+            cmd=`zenity --entry --title="$prog" --text="Insert the command"`
+            echo "Exec=$cmd" >> $name.desktop
+            ;;
         "URL address")
-        	set_borwser
-        	dato=`zenity --entry --title="$prog" --text="Insert the address"`
-        	echo "Exec=$b $dato" >>$nome.desktop
-        ;;
-        esac
-        zenity --question --title="$prog" --text="Does the launcher start in a terminal?"
-        if [ $? -eq 0 ]
-        then
-                echo "Terminal=true" >>$nome.desktop
-        else
-                echo "Terminal=false" >>$nome.desktop
-        fi
-        c=`zenity --list --title="$prog" --text="Choose the category" --column="Options: " "AudioVideo" "Audio" "Video" "Development" "Education"\
+            set_browser
+            cmd=`zenity --entry --title="$prog" --text="Insert the address"`
+            echo "Exec=$b $cmd" >> $name.desktop
+            ;;
+    esac
+    zenity --question --title="$prog" --text="Does the launcher start in a terminal ?"
+    if [ $? -eq 0 ]
+    then
+        echo "Terminal=true"  >> $name.desktop
+    else
+        echo "Terminal=false" >> $name.desktop
+    fi
+    c=`zenity --list --title="$prog" --text="Choose the category" --column="Options: " "AudioVideo" "Audio" "Video" "Development" "Education"\
         "Game" "Graphics" "Network" "Office" "Settings" "System"`
-        echo "Categories=$c" >>$nome.desktop
+    echo "Categories=$c" >> $name.desktop
 }
 
-##################################### Scritpt #####################################
-#Read the name of the launcher
-nome=`zenity --entry --title="$prog" --text="Insert the name of your launcher"`
-if [ "$nome" = '' ]
+
+## Script
+
+# Read the name of the launcher
+name=`zenity --entry --title="$prog" --text="Insert the name of your launcher"`
+if [ "$name" = '' ]
 then
-	echo "Impossible to create launcher"
-	zenity --error --title="$prog" --text="Impossible to create the launcher without the name"
-	exit 1
+    echo "Impossible to create a launcher without a name"
+    zenity --error --title="$prog" --text="Impossible to create a launcher without a name"
+    exit 1
 fi
-#Create the file
-touch $nome.desktop
+
+# Create the file
+touch $name.desktop
 file_desktop
-chmod +x $nome.desktop
+chmod +x $name.desktop
 zenity --question --title="$prog" --text="Do you want to add this launcher to your desktop?"
 if [ $? -eq 0 ]
 then
-	if [ -e ~/Desktop ]
-	then
-		cp $nome.desktop ~/Desktop/
-	else
-		cp $nome.desktop ~/Scrivania/
-	fi
+    if [ -e ~/Desktop ]
+    then
+        cp $name.desktop ~/Desktop/
+    else
+        cp $name.desktop ~/Scrivania/
+    fi
 fi
-mv $nome.desktop ~/.local/share/applications/
+mv $name.desktop ~/.local/share/applications/
 if [ $? -eq 0 ]
 then
-	zenity --info --title="$prog" --text="Launcher successfully created"
+    zenity --info --title="$prog" --text="Launcher successfully created"
 else
-	zenity --error --title="$prog" --text="The launcher cannot be created"
+    zenity --error --title="$prog" --text="The launcher cannot be created"
 fi
--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-#This is a script that install LinkIt automatically
-#This script must be executed as root
+
+# This script installs LinkIt automatically
+# It must be executed as root
+
 cp linkit.sh /usr/bin/
 cp -R etc/linkit/ /etc/
 cp applications/linkit.desktop /usr/share/applications
 cp applications/linkit.png /usr/share/icons/hicolor/48x48/
-#Configuration finished
-echo "Configuration succesflully finished"
-echo "Lunch linkit from terminal with linkit.sh or find it in your menù"
+
+echo "Installation successful."
+echo "Launch LinkIt from terminal with linkit.sh or find it in your menu."
